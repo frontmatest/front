79c8310237d759c5ebc4fe1019c8c79129d99533
frontmatest <44202768+frontmatest@users.noreply.github.com>
2018-10-16 16:51:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 <h1>Hello everybody! </h1>
-=======
-<h1>Hello Souf & Mahdi ! </h1>
-<<<<<<< HEAD
->>>>>>> feature1
-=======
->>>>>>> add Mahdi
->>>>>>> 396f92f538ac78ed889ed47a2a64a3e302a021d9
